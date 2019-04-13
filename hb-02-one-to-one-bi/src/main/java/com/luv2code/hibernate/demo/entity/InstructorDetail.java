63b314358276d2960c4a8c6ec616310a8d4cba2d
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-04-13 11:55:15 -0400

    Use bi-directional mapping to retrieve both objects

--- hb-02-one-to-one-bi/src/main/java/com/luv2code/hibernate/demo/entity/InstructorDetail.java
+++ hb-02-one-to-one-bi/src/main/java/com/luv2code/hibernate/demo/entity/InstructorDetail.java
@@ -1,10 +1,12 @@
 package com.luv2code.hibernate.demo.entity;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -32,6 +34,12 @@ public class InstructorDetail {
 
 	@Column(name = "hobby")
 	private String hobby;
+	
+	// using this hibernate will go to Instructor class and
+	// search for "instructorDetail" property and then try to figure out the relationship
+	// CascadeType.All transfers the action to the object in the relationship
+	@OneToOne(mappedBy = "instructorDetail", cascade = CascadeType.ALL)
+	private Instructor instructor;
 
 	public InstructorDetail() {
 	}
@@ -63,6 +71,14 @@ public class InstructorDetail {
 
 	public void setHobby(String hobby) {
 		this.hobby = hobby;
+	}
+
+	public Instructor getInstructor() {
+		return instructor;
+	}
+
+	public void setInstructor(Instructor instructor) {
+		this.instructor = instructor;
 	}
 
 	@Override
