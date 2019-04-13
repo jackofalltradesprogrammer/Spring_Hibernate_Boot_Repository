3a5601dcc980cfee0b3203bf21b056778f14c79a
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-04-13 09:48:26 -0400

    Initail configuration for Hibernate Mapping project

---
+++ hibernate-tutorial/src/main/java/com/luv2code/hibernate/demo/UpdateStudentDemo.java
@@ -0,0 +1,45 @@
+package com.luv2code.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.luv2code.hibernate.demo.entity.Student;
+
+public class UpdateStudentDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Student.class)
+				.buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			int studentId = 1;
+			session.beginTransaction();
+
+			// retrieve student based on the id: primary key
+//			System.out.println("\nGetting student with id" + studentId);
+//			Student myStudent = session.get(Student.class, studentId);
+//
+//			System.out.println("Deleteing student...");
+//			session.delete(myStudent);
+			session.createQuery("delete from Student where id=2").executeUpdate();
+
+			// commit the transaction
+			session.getTransaction().commit(); // This updates in the database
+
+			System.out.println("Done!");
+		} catch (Exception e) {
+			System.out.println(e);
+		} finally {
+			factory.close();
+		}
+
+	}
+
+}
