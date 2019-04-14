ab16aa41633fccb1e5a54f0d6e5c89642e4b9142
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-04-14 17:36:32 -0400

    Create OneToMany and ManyToOne between Instructor and course

--- hb-03-one-to-many/src/main/java/com/luv2code/hibernate/demo/entity/Instructor.java
+++ hb-03-one-to-many/src/main/java/com/luv2code/hibernate/demo/entity/Instructor.java
@@ -1,5 +1,8 @@
 package com.luv2code.hibernate.demo.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -7,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
@@ -34,6 +38,12 @@ public class Instructor {
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "instructor_detail_id")
 	private InstructorDetail instructorDetail;
+
+	// mappedBy tells hibernate to go "Course" class and check "instructor" property
+	// to determine the relationship between the two
+	@OneToMany(mappedBy = "instructor", cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
+			CascadeType.REFRESH })
+	private List<Course> courses;
 
 	public Instructor() {
 		// TODO Auto-generated constructor stub
@@ -83,6 +93,24 @@ public class Instructor {
 
 	public void setInstructorDetail(InstructorDetail instructorDetail) {
 		this.instructorDetail = instructorDetail;
+	}
+
+	public List<Course> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Course> courses) {
+		this.courses = courses;
+	}
+	
+	// add convenience methods for bi-directional relationship
+	// need to initialize courses
+	public void add(Course tempCourse) {
+		if (courses == null) {
+			courses = new ArrayList<>();
+		}
+		courses.add(tempCourse);
+		tempCourse.setInstructor(this); // as the method is called from the Instructor object, we can pass this
 	}
 
 	@Override
