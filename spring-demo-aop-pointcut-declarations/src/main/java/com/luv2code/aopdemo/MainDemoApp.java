0666bb2947fc4e754db7f8f327f4eb11a736c9b2
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-04-21 19:38:41 -0400

    initial commit for AOP: Pointcut Declarations

---
+++ spring-demo-aop-pointcut-declarations/src/main/java/com/luv2code/aopdemo/MainDemoApp.java
@@ -0,0 +1,31 @@
+package com.luv2code.aopdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.luv2code.aopdemo.dao.AccountDAO;
+import com.luv2code.aopdemo.dao.MembershipDAO;
+
+public class MainDemoApp {
+
+	public static void main(String[] args) {
+		
+		// read spring config java class
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DemoConfig.class);
+		
+		// get the bean from spring container
+		AccountDAO theAccountDAO = context.getBean("accountDAO", AccountDAO.class);
+		MembershipDAO theMembershipDAO = context.getBean("membershipDAO", MembershipDAO.class);
+		
+		// call the business method on which we setup a @Before advice
+		Account myAccount = new Account();
+		theAccountDAO.addAccount(myAccount, true);
+		theAccountDAO.doWork();
+//		theMembershipDAO.addAccount();
+		theMembershipDAO.addSillyMember();
+		theMembershipDAO.goToSleep();
+		
+		// close the context
+		context.close();
+	}
+
+}
