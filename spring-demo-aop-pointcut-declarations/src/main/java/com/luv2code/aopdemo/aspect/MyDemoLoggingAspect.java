4ba9a6f44aeab481fb773f62a55ef7219089ce7b
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-04-21 19:44:47 -0400

    PointCut declarations applying on multiple advices

--- spring-demo-aop-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-pointcut-declarations/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,18 +2,25 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
 	
-	// this is where we add all of our related advice for logging
-	// let's start with a @Before advice
+	// this is a pointCut declaration that can be used on multiple advices
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	private void forDaoPackage() {}
 	
-	@Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+	@Before("forDaoPackage()")
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n=====>>> Executing @Before advice on method()");
+	}
+	
+	@Before("forDaoPackage()")
+	public void performApiAnalytics() {
+		System.out.println("\n====>>>> Performing API analytics");
 	}
 	
 	
