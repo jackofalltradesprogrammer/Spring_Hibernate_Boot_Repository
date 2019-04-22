b918886551abc189fcc688f635d69308e1ae78f7
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-04-21 20:39:41 -0400

    Using AOP: JoinPoint, we can log method signature and arguments

--- spring-demo-aop-read-joinpoint/src/main/java/com/luv2code/aopdemo/MainDemoApp.java
+++ spring-demo-aop-read-joinpoint/src/main/java/com/luv2code/aopdemo/MainDemoApp.java
@@ -18,6 +18,8 @@ public class MainDemoApp {
 		
 		// call the business method on which we setup a @Before advice
 		Account myAccount = new Account();
+		myAccount.setName("Madhu");
+		myAccount.setLevel("Platinum");
 		theAccountDAO.addAccount(myAccount, true);
 		theAccountDAO.doWork();
 		
--- spring-demo-aop-read-joinpoint/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ spring-demo-aop-read-joinpoint/src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -1,9 +1,13 @@
 package com.luv2code.aopdemo.aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import com.luv2code.aopdemo.Account;
 
 @Aspect
 @Component
@@ -11,8 +15,27 @@ import org.springframework.stereotype.Component;
 public class MyDemoLoggingAspect {
 
 	@Before("com.luv2code.aopdemo.aspect.LuvAopExpressions.forDaoPackageNoGetterSetter()")
-	public void beforeAddAccountAdvice() {
+	public void beforeAddAccountAdvice(JoinPoint theJoinPoint) {
 		System.out.println("\n=====>>> Executing @Before advice on method()");
+		
+		// display the method signature using JoinPoint which gives us the metadata about method call
+		MethodSignature methodSig = (MethodSignature) theJoinPoint.getSignature();
+		System.out.println("Method: " + methodSig);
+		
+		// display method arguments
+		// get args
+		Object[] args = theJoinPoint.getArgs();
+				
+		// loop through args
+		for(Object tempArg : args) {
+			System.out.println(tempArg);
+			if (tempArg instanceof Account) {
+				// downcast and print Account specific stuff
+				Account theAccount = (Account) tempArg;
+				System.out.println("account name: " + theAccount.getName());
+				System.out.println("account level: " + theAccount.getLevel());
+			}
+		}
 	}
 
 }
