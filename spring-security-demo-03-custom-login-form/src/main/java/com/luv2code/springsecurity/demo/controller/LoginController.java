da02267d01b0e496a414b0f8cbdc2f2fa4a80c97
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-04-25 16:20:04 -0400

    Spring security - add a custom access denied page

--- spring-security-demo-03-custom-login-form/src/main/java/com/luv2code/springsecurity/demo/controller/LoginController.java
+++ spring-security-demo-03-custom-login-form/src/main/java/com/luv2code/springsecurity/demo/controller/LoginController.java
@@ -11,4 +11,11 @@ public class LoginController {
 		
 		return "fancy-login";
 	}
+	
+	// Add request mapping for /access denied
+	@GetMapping("/accessDenied")
+	public String showAccessDenied() {
+		
+		return "access-denied";
+	}
 }
