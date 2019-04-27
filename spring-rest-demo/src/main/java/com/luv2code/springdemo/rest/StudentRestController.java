67a09cdd4a3005249e83742742a76a919f34ba01
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-04-27 11:27:54 -0400

    Using @PathVariable - return only 1 student from the REST API

--- spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/luv2code/springdemo/rest/StudentRestController.java
@@ -3,6 +3,9 @@ package com.luv2code.springdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +15,29 @@ import com.luv2code.springdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 	
+	private List<Student> theStudents;
+	
+	// define @PostConstruct to load the student data ... only once!
+	@PostConstruct
+	public void loadData() {
+		theStudents = new ArrayList<Student>();
+		theStudents.add(new Student("Poornima", "Patel"));
+		theStudents.add(new Student("Mario", "Rossi"));
+		theStudents.add(new Student("Mary", "Smith"));
+	}
+	
 	// define endpoint for "/students" - return list of students
 	@RequestMapping("/students")
 	public List<Student> getStudents() {
 		
-		List <Student> theStudents = new ArrayList<Student>();
-		theStudents.add(new Student("Poornima", "Patel"));
-		theStudents.add(new Student("Mario", "Rossi"));
-		theStudents.add(new Student("Mary", "Smith"));
 		return theStudents;
 	}
+	
+	// define endpoint to return a student at index
+	@RequestMapping("/students/{studentId}")
+	public Student getStudent(@PathVariable int studentId)  {
+		
+		return theStudents.get(studentId);
+	}
+	
 }
